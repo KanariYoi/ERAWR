61e2b904675ffd76579c1fe1230677174cbf24ff
KanariYoi <132283772+KanariYoi@users.noreply.github.com>
2025-07-14 23:38:04 +0800

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,1 @@
-window.API_BASE_URL =
-  window.location.hostname === "localhost"
-    ? "http://127.0.0.1:8000"
-    : "https://erawr.onrender.com";
+window.API_BASE_URL = "https://erawr.onrender.com";
